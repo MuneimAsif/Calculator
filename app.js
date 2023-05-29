267b5a5255c2a843b2c6a60095796b1a779f96dd
MuneimAsif <muneimasif2004@gmail.com>
2023-05-29 06:24:11 -0700

    Changing Shapes Of A Box

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-var display = document.getElementById("display")
-function getVal (num){
-    display.value += num
-
-}
-function calVal (){
-    display.value = eval(display.value)
-}
-function clearVal (){
-    display.value = ""
+var shape = document.getElementById('shapsChange')
+function changingClass(clasS){
+shapeChange.className = clasS
 }
